f2ea10429e6b2329a8e1cd2b8cc23b9a7dbc8df6
Christopher <19462057@colima.tecnm.mx>
2023-12-06 14:50:12 -0600

    Add the password´s secrets of Mysql, connect it in dbconnection and create db_password and db_root_password files

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -1,11 +1,17 @@
+const fs = require('fs');
 const knex = require('knex');
-const db = knex({
-    client: 'mysql2',
-    connection: {
-        host: 'mysql',  // Nombre del servicio en el docker-compose.yml
-        user: 'user_videojuego',
-        password: '54321',
-        database: 'videojuego'
-    }
-});
-module.exports = db;
+
+const connectToMysql = async function(){
+    const dbPassword = await fs.promises.readFile('/run/secrets/db_password');
+    const db = knex({
+        client: 'mysql2',
+        connection: {
+            host: 'mysql',  // Nombre del servicio en el docker-compose.yml
+            user: 'user_videojuego',
+            password: dbPassword,
+            database: 'videojuego'
+        }
+    });
+    return db;
+}
+module.exports = {connectToMysql};
